231b65a97002d7dad6b0d8faf03af223ac56771b
Terseo <44824248+Terseo@users.noreply.github.com>
2022-07-01 11:25:50 -0400

    Revert Example Test

--- src/test/java/com/unosquare/FirstAPITest.java
+++ src/test/java/com/unosquare/FirstAPITest.java
@@ -2,37 +2,36 @@ package com.unosquare;
 
 import org.testng.annotations.Test;
 import static org.hamcrest.Matchers.equalTo;
-import io.restassured.http.ContentType;
-import static io.restassured.RestAssured.*;
+
+import io.restassured.RestAssured;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
 
 public class FirstAPITest {
- 
-
-  
   @Test
-  public void f_reqres_in() {
+  public void f() {
 	  
-	  given()
-	  .when()
-	  	.get("https://reqres.in/api/users/2")
-	  		.then()
-	  		.assertThat().statusCode(200)
-	  		.assertThat().contentType(ContentType.JSON)
-	  		.assertThat().body("data.id", equalTo(2))
-	  		.assertThat().body("data.email", equalTo("janet.weaver@reqres.in"))
-	  		.assertThat().body("data.first_name", equalTo("Janet"))
-	  		.assertThat().body("data.last_name", equalTo("Weaver"))
-	  		.assertThat().body("data.avatar", equalTo("https://reqres.in/img/faces/2-image.jpg"))
-	  		
-	  		.assertThat().body("support.url", equalTo("https://reqres.in/#support-heading"))
-	  		.assertThat().body("support.text", equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
-	  
-	  Reporter.log("Sucess response 200 validation");
-}
+	    RestAssured.baseURI = "https://reqres.in/api/";
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get("/users/2");
+		
+		int statusCode = response.getStatusCode();
+
+		// Assert that correct status code is returned.
+		Assert.assertEquals(statusCode,200);
+		Reporter.log("Sucess 200 validation");
+		
+		response.then().body("data.first_name", equalTo("Janet"));
+		Reporter.log(response.body().asString());
+		
+  }
+  
   @BeforeMethod
   public void beforeMethod() {
   }
