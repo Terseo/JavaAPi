e8b6a01db2b450a4e2946614cc80913bd7f26844
Terseo <44824248+Terseo@users.noreply.github.com>
2022-07-01 11:16:56 -0400

    format

--- src/test/java/com/unosquare/FirstAPITest.java
+++ src/test/java/com/unosquare/FirstAPITest.java
@@ -16,22 +16,17 @@ public class FirstAPITest {
   @Test
   public void f_reqres_in() {
 	  
-	  given()
-	  .when()
-	  	.get("https://reqres.in/api/users/2")
-	  		.then()
-	  		.assertThat().statusCode(200)
-	  		.assertThat().contentType(ContentType.JSON)
-	  		.assertThat().body("data.id", equalTo(2))
-	  		.assertThat().body("data.email", equalTo("janet.weaver@reqres.in"))
-	  		.assertThat().body("data.first_name", equalTo("Janet"))
-	  		.assertThat().body("data.last_name", equalTo("Weaver"))
-	  		.assertThat().body("data.avatar", equalTo("https://reqres.in/img/faces/2-image.jpg"))
-	  		
-	  		.assertThat().body("support.url", equalTo("https://reqres.in/#support-heading"))
-	  		.assertThat().body("support.text", equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
-	  
-	  Reporter.log("Sucess 200 validation");
+		given().when().get("https://reqres.in/api/users/2").then().assertThat().statusCode(200).assertThat()
+				.contentType(ContentType.JSON).assertThat().body("data.id", equalTo(2)).assertThat()
+				.body("data.email", equalTo("janet.weaver@reqres.in")).assertThat()
+				.body("data.first_name", equalTo("Janet")).assertThat().body("data.last_name", equalTo("Weaver"))
+				.assertThat().body("data.avatar", equalTo("https://reqres.in/img/faces/2-image.jpg"))
+
+				.assertThat().body("support.url", equalTo("https://reqres.in/#support-heading")).assertThat()
+				.body("support.text",
+						equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
+
+		Reporter.log("Sucess response 200 validation");
 }
   @BeforeMethod
   public void beforeMethod() {
