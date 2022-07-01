87d1df6f0263779e8deb1878d298f71886025621
Terseo <44824248+Terseo@users.noreply.github.com>
2022-07-01 12:04:21 -0400

    Adding Second Exercise

--- src/test/java/com/unosquare/FirstAPITest.java
+++ src/test/java/com/unosquare/FirstAPITest.java
@@ -29,6 +29,27 @@ public class FirstAPITest {
 	  
 	  Reporter.log("Sucess 200 validation");
 }
+  
+  @Test
+  public void f_reqres_unknown() {
+	  
+	  given()
+	  .when()
+	  	.get("https://reqres.in/api/unknown/2")
+	  		.then()
+	  		.assertThat().statusCode(200)
+	  		.assertThat().contentType(ContentType.JSON)
+	  		.assertThat().body("data.id", equalTo(2))
+	  		.assertThat().body("data.name", equalTo("fuchsia rose"))
+	  		.assertThat().body("data.year", equalTo(2001))
+	  		.assertThat().body("data.color", equalTo("#C74375"))
+	  		.assertThat().body("data.pantone_value", equalTo("17-2031"))
+	  		
+	  		.assertThat().body("support.url", equalTo("https://reqres.in/#support-heading"))
+	  		.assertThat().body("support.text", equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
+	  
+	  Reporter.log("Sucess 200 validation");
+}
   @BeforeMethod
   public void beforeMethod() {
   }
